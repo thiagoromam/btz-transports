edfd16bfc53708941f8750d17ef618897d175b19
Thiago Romam <thiago.romam@outlook.com>
2023-12-13 18:37:06 -0300

    revisão de menus

--- BtzTransports.Web/Controllers/HomeController.cs
+++ BtzTransports.Web/Controllers/HomeController.cs
@@ -4,23 +4,6 @@ namespace BtzTransports.Web.Controllers
 {
     public class HomeController : BaseMvcController
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        public ActionResult Index() => View();
     }
 }
