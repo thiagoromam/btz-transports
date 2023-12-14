8a418e670837e436199e91f52ae9d41afd2e8385
Thiago Romam <thiago.romam@outlook.com>
2023-12-14 11:40:38 -0300

    correções gerais visuais e redirecionamento

--- BtzTransports.Web/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.Mvc;
-
-namespace BtzTransports.Web.Controllers
-{
-    public class HomeController : BaseMvcController
-    {
-        public ActionResult Index() => View();
-    }
-}
